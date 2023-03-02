3afabcc65c02fe7f50399a9c791ffe11cca6b0d2
Dayana <Dayana@DESKTOP-BMET23Q>
2023-03-01 19:20:19 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio1 calculadora/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+   [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+    namespace ejercicio1_calculadora 
+{ 
+
+
+        public partial class App : Application
+        {
+            public App()
+            {
+                InitializeComponent();
+
+                MainPage = new MainPage();
+            }
+
+            protected override void OnStart()
+            {
+                
+            }
+
+            protected override void OnSleep()
+            {
+                
+            }
+
+            protected override void OnResume()
+            {
+                
+            }
+        }
+    }
+
+
